8cead7c3f287ef71957dbc0743d59f3a67a2126c
Yuki <1154952012@qq.com>
2023-12-05 18:56:52 +0800

    防止缓存击穿，添加了单体过滤器

--- cache/myCache.go
+++ cache/myCache.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"myCache/rest"
+	"myCache/singleflight"
 	"sync"
 )
 
@@ -22,6 +23,7 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     rest.PeerPicker
+	loader    *singleflight.Group
 }
 
 var (
@@ -41,6 +43,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		mainCache: cache{
 			cacheBytes: cacheBytes,
 		},
+		loader: &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -70,15 +73,21 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
 			}
-			log.Println("[GeeCache] Failed to get from peer", err)
 		}
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+	return
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
