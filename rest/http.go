ef3b1cb3ed3742da31cb226d9f31fa627a35aaa1
Yuki <1154952012@qq.com>
2023-12-05 16:45:36 +0800

    分布式节点

--- rest/http.go
+++ rest/http.go
@@ -4,21 +4,29 @@ import (
 	"fmt"
 	"log"
 	"myCache/cache"
+	"myCache/consistenthash"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_myCache/"
+const (
+	defaultBasePath = "/_myCache/"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	self     string //记录自己的地址，包括主机名/IP和端口
-	BasePath string //作为节点间通讯地址的前缀，默认为 "/_myCache/"
+	self        string //记录自己的地址，包括主机名/IP和端口
+	basePath    string //作为节点间通讯地址的前缀，默认为 "/_myCache/"
+	mu          sync.Mutex
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter
 }
 
 func NewHTTPPool(self string) *HTTPPool {
 	return &HTTPPool{
 		self:     self,
-		BasePath: defaultBasePath,
+		basePath: defaultBasePath,
 	}
 }
 
@@ -27,11 +35,11 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 }
 
 func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
-	if !strings.HasPrefix(r.URL.Path, p.BasePath) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
-	parts := strings.SplitN(r.URL.Path[len(p.BasePath):], "/", 2)
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
@@ -54,3 +62,27 @@ func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Write(view.ByteSlice())
 }
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+var _ PeerGetter = (*httpGetter)(nil)
